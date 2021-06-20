23e25c31a62985734b297918252d4feb2a37c4ad
Akira Yumiyama <yumiyama.akira@gmail.com>
2021-06-20 21:55:18 +0900

    テスト実行結果が読みやすい形に console.log を整理

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { VendingMachine } from './VendingMachine';
 
 const scenario1 = () => {
-    console.log('[1] 100円を入れて 0: COKE を購入');
     const money = 100;
     const drinkType = 0;
     const machine = new VendingMachine();
@@ -9,17 +8,16 @@ const scenario1 = () => {
     const change = machine.refund();
 
     console.log(
-        '結果',
+        '[1] 100円を入れて 0: COKE を購入',
         {
-            boughtDrink: myDrink,
-            change: change,
+            [`購入したドリンク`]: myDrink,
+            [`お釣り`]: change,
         },
         '→ Cokeを購入して、お釣りは0円'
     );
 };
 
 const scenario2 = () => {
-    console.log('[2] 200円を入れて 0: COKE を購入');
     const money = 200;
     const drinkType = 0;
     const machine = new VendingMachine();
@@ -27,17 +25,16 @@ const scenario2 = () => {
     const change = machine.refund();
 
     console.log(
-        '結果',
+        '[2] 200円を入れて 0: COKE を購入',
         {
-            boughtDrink: myDrink,
-            change: change,
+            [`購入したドリンク`]: myDrink,
+            [`お釣り`]: change,
         },
         '→ 100円か500円しか受け付けないので、購入できず、そのまま200円が返ってくる'
     );
 };
 
 const scenario3 = () => {
-    console.log('[3] 500円を入れて 2: TEA を購入');
     const money = 500;
     const drinkType = 2;
     const machine = new VendingMachine();
@@ -45,17 +42,16 @@ const scenario3 = () => {
     const change = machine.refund();
 
     console.log(
-        '結果',
+        '[3] 500円を入れて 2: TEA を購入',
         {
-            boughtDrink: myDrink,
-            change: change,
+            [`購入したドリンク`]: myDrink,
+            [`お釣り`]: change,
         },
         '→ TEAが出て、400円がお釣りとして返ってくる'
     );
 };
 
 const scenario4 = () => {
-    console.log('[4] 100円を入れて 1: DietCoke を購入 を 6回繰り返す');
     const money = 100;
     const drinkType = 1;
     const machine = new VendingMachine();
@@ -63,37 +59,41 @@ const scenario4 = () => {
     const results = [
         // 1回目
         {
-            drink: machine.buy(money, drinkType),
-            change: machine.refund(),
+            [`購入したドリンク`]: machine.buy(money, drinkType),
+            [`お釣り`]: machine.refund(),
         },
         // 2回目
         {
-            drink: machine.buy(money, drinkType),
-            change: machine.refund(),
+            [`購入したドリンク`]: machine.buy(money, drinkType),
+            [`お釣り`]: machine.refund(),
         },
         // 3回目
         {
-            drink: machine.buy(money, drinkType),
-            change: machine.refund(),
+            [`購入したドリンク`]: machine.buy(money, drinkType),
+            [`お釣り`]: machine.refund(),
         },
         // 4回目
         {
-            drink: machine.buy(money, drinkType),
-            change: machine.refund(),
+            [`購入したドリンク`]: machine.buy(money, drinkType),
+            [`お釣り`]: machine.refund(),
         },
         // 5回目
         {
-            drink: machine.buy(money, drinkType),
-            change: machine.refund(),
+            [`購入したドリンク`]: machine.buy(money, drinkType),
+            [`お釣り`]: machine.refund(),
         },
         // 6回目
         {
-            drink: machine.buy(money, drinkType),
-            change: machine.refund(),
+            [`購入したドリンク`]: machine.buy(money, drinkType),
+            [`お釣り`]: machine.refund(),
         },
     ];
 
-    console.log('結果', results, '→ 最後の6回目は DietCoke が出なくて、投入した100円がそのまま返ってくる');
+    console.log(
+        '[4] 100円を入れて 1: DietCoke を購入 を 6回繰り返す',
+        results,
+        '→ 最後の6回目は DietCoke が出なくて、投入した100円がそのまま返ってくる'
+    );
 };
 
 export { scenario1, scenario2, scenario3, scenario4 };
